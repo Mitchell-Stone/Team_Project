8b2697fa0c99c2334fd54f578913faf82194fd36
TAFE\0111005906 <mitch_010@hotmail.com>
2018-05-28 10:03:04 +1000

    Added a courses.java file into beans. Also added some code to create the courses list for the student that is logged in.

--- eits/src/student/StudentDashboardController.java
+++ eits/src/student/StudentDashboardController.java
@@ -5,9 +5,17 @@
  */
 package student;
 
+import beans.Courses;
+import beans.Student;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +24,41 @@ import javafx.fxml.Initializable;
  */
 public class StudentDashboardController implements Initializable {
 
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TableView<Courses> tableView;
+    @FXML
+    private TableColumn<Courses, String> courseName;
+    @FXML
+    private TableColumn<Courses, String> industry;
+    @FXML
+    private TableColumn<Courses, String> location;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        courseName.setCellValueFactory(new PropertyValueFactory<Courses, String>("name"));
+        industry.setCellValueFactory(new PropertyValueFactory<Courses, String>("industry"));
+        location.setCellValueFactory(new PropertyValueFactory<Courses, String>("location"));
+        
+        tableView.getItems().setAll(parseCoursesList());
+    }
+    
+    public List<Courses> parseCoursesList(){
+        
+        //execute SQL statement to get all course for the studen that logged in
+        
+        //create a for loop to iterate through the courses to populate the list
+        
+        //return courses;
+        
+    }
     
 }
