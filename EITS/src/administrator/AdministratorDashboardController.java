304a88352439f19dda602da56f5a52cb91659890
TAFE\0111005906 <mitch_010@hotmail.com>
2018-05-31 13:45:35 +1000

    Finished the database query to get all students. Got the admin dashboard to print the student object ids to the console

--- EITS/src/administrator/AdministratorDashboardController.java
+++ EITS/src/administrator/AdministratorDashboardController.java
@@ -6,13 +6,17 @@
 package administrator;
 
 import beans.Student;
+import beans.User;
 import java.net.URL;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import model.MainModel;
 
 /**
  * FXML Controller class
@@ -33,12 +37,19 @@ public class AdministratorDashboardController implements Initializable {
     }    
 
     @FXML
-    private void showAllStudents(MouseEvent event) {
+    private void showAllStudents(MouseEvent event) throws SQLException {
+        System.out.println("You Clicked me dawg");
         
-        MainModel model = new MainModel();
-        
-        List<Student> = model.getAllStudents("Students");
-        
+        try{
+            MainModel model = new MainModel();
+            ArrayList<Student> list = model.getAllStudents();
+            
+            
+            
+            System.out.println(list);
+        }
+        catch(NullPointerException ex){
+            System.out.println("Null Pointer Exception");
+        } 
     }
-    
 }
--- EITS/src/model/MainModel.java
+++ EITS/src/model/MainModel.java
@@ -1,8 +1,48 @@
 
 package model;
 
+import beans.Student;
+import beans.User;
+import db.DbType;
+import db.DbUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+
 public class MainModel {
     
-    
-    
+    public ArrayList<Student> getAllStudents() throws SQLException{
+        
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        
+        ResultSet rs = null;
+
+        //execute query to get all students
+        String query = "SELECT * FROM student";
+
+        try{
+            Connection conn = DbUtil.getConn(DbType.MYSQL);
+            PreparedStatement stmt = conn.prepareStatement(query);           
+            
+            Student student;
+            
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                student = new Student(rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"));
+                studentList.add(student);
+            }            
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+        }  
+        return studentList;
+    }  
 }
