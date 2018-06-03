83018c6a0bf44f7be2c60242790b0ff7ee3494a0
DESKTOP-VKOA92J\mitch <mitch_010@hotmail.com>
2018-06-03 13:05:13 +1000

    You can now add a new user of any type from the admin dashboard. When the user is added you are returned to the dashboard.

--- eits/src/globalInterfaces/AddNewUserController.java
+++ eits/src/globalInterfaces/AddNewUserController.java
@@ -5,15 +5,24 @@
  */
 package globalInterfaces;
 
+import beans.User;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import model.MainModel;
 
 /**
  * FXML Controller class
@@ -26,7 +35,19 @@ public class AddNewUserController implements Initializable {
     private ComboBox cb_tableSelection;
     @FXML
     private Button btn_addUser;
+    @FXML
+    private TextField tf_firstName;
+    @FXML
+    private TextField tf_lastName;
+    @FXML
+    private TextField tf_email;
+    @FXML
+    private TextField tf_password;
 
+    private String windowURL = "/administrator/administratorDashboard.fxml";
+    @FXML
+    private Button btn_returnToDashboard;
+    
     /**
      * Initializes the controller class.
      */
@@ -38,13 +59,30 @@ public class AddNewUserController implements Initializable {
             "Case Worker",
             "Administrator"
         );
-        
         cb_tableSelection.getItems().addAll(options);
          
     }    
 
     @FXML
-    private void btn_addUser(MouseEvent event) {
-    }
-    
+    private void btn_addUser(MouseEvent event) throws SQLException, IOException {
+        
+        String table = cb_tableSelection.getSelectionModel().getSelectedItem().toString();
+        if (table != null) {
+            User user = new User();
+            user.setTable(table);
+            user.setFirstName(tf_firstName.getText());
+            user.setLastName(tf_lastName.getText());
+            user.setEmail(tf_email.getText());
+            user.setPassword(tf_password.getText());
+
+            MainModel.addNewUser(user);
+
+            Stage stage = (Stage) btn_addUser.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource(windowURL));
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }  
 }
